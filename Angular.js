c5f5985366648695c0eae977bb06caebd84cc081
Margarita <margaritasadov@gmail.com>
2016-06-08 11:11:42 +0300

    Fixed issues

--- Angular.js
+++ Angular.js
@@ -11,8 +11,8 @@ app.controller('myCtrl', function($scope, $http){
 	};
     //$scope.map.markers.push({"latitude":33.22,"longitude":35.33});
     //$scope.map.markers.push({"latitude":50.4388394,"longitude":30.5147999});
-    $scope.map.markers.push({"id":1000,"coords":{"latitude":33.22,"longitude":35.33}});
-    $scope.map.markers.push({"id":1001,"coords":{"latitude":50.4388394,"longitude":30.5147999}});
+    //$scope.map.markers.push({"id":1000,"coords":{"latitude":33.22,"longitude":35.33}});
+    //$scope.map.markers.push({"id":1001,"coords":{"latitude":50.4388394,"longitude":30.5147999}});
 	$http.get('http://clean.intent.im:80/api/point').
 		success(function(data) {
 			$scope.points = data;
