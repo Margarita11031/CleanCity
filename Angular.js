202a3270c0bd287af75422c6a421f7e5a3b8aa5f
Margarita <margaritasadov@gmail.com>
2016-06-08 10:50:10 +0300

    Added markers to the map

--- Angular.js
+++ Angular.js
@@ -3,15 +3,26 @@
 var app= angular.module("angCleanCity",['uiGmapgoogle-maps']);
 
 app.controller('myCtrl', function($scope, $http){
-		$http.get('http://clean.intent.im:80/api/point').
-        success(function(data) {
-            $scope.points = data;
-        });	
+	$scope.map = {
+		center: { latitude: 50.4388394, longitude: 30.5147999 },
+		zoom: 13,
+		markers: [],
+		fit: true
+	};
+    //$scope.map.markers.push({"latitude":33.22,"longitude":35.33});
+    //$scope.map.markers.push({"latitude":50.4388394,"longitude":30.5147999});
+    $scope.map.markers.push({"id":1000,"coords":{"latitude":33.22,"longitude":35.33}});
+    $scope.map.markers.push({"id":1001,"coords":{"latitude":50.4388394,"longitude":30.5147999}});
+	$http.get('http://clean.intent.im:80/api/point').
+		success(function(data) {
+			$scope.points = data;
+			//for(var i=0; i<$scope.points.length; i++){
+			for(var i=0; i<50; i++){
+				var tblXYZCoords = $scope.points[i].location.split(",");
+				$scope.map.markers.push({"id":$scope.points[i].id,"coords":{"latitude":tblXYZCoords[1],"longitude":tblXYZCoords[0]}});
+			}
+		});
 
-
-		$scope.map = { 
-			center: { latitude: 50.4388394, longitude: 30.5147999 },
-			zoom: 13 };
 	});
 	
 	 
